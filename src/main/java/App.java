0dbfbb3d5ef717a1353701da3960dfedc3d272ea
Guest User <Guest@Epicodus-5D.local>
2017-08-30 15:57:16 -0700

    add css styles, style content

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,13 @@
+import com.google.gson.Gson;
+import dao.Sql2oPlantDao;
+import models.Plant;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
@@ -10,12 +16,23 @@ import static spark.Spark.staticFileLocation;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        Sql2oPlantDao plantDao;
+        Connection conn;
+        Gson gson = new Gson();
 
+        //String connectionString = "jdbc:h2:~/recipe-box.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        String connectionString = ("jdbc:postgresql://localhost:5432/garden_guide");
+
+        Sql2o sql2o = new Sql2o(connectionString, null, null);
+        plantDao = new Sql2oPlantDao(sql2o);
+        conn = sql2o.open();
 
 
         // show home page (root route)
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+            List<Plant> plants = plantDao.getAll();
+            model.put("plants", plants);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -36,6 +53,13 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "careers.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //get: show API Information page
+        get("/api_documentation", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "endpoints.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
         //get: show careers page
         get("/plantDetail/:id", (req, res) -> {
